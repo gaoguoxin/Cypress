e9c1135139281d938cae6dbe7707fe2651f23ad7
Davis Gao <naitnix@126.com>
2014-12-22 16:16:16 +0800

    m

--- app/uploaders/journalisms_uploader.rb
+++ app/uploaders/journalisms_uploader.rb
@@ -38,8 +38,12 @@ class JournalismsUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [80, 80]
+    process :resize_to_fill => [80, 80]
   end
+
+  version :middle do
+    process :resize_to_fill => [258, 160]
+  end  
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
